dbfbc4846c68a429096a8f221269eec70265d3cb
Rustam Kuramshin <leo.in.empty@gmail.com>
2017-12-01 02:26:24 +0300

    Del pass

--- DataCollection.cmd
+++ DataCollection.cmd
@@ -1,14 +1,14 @@
 @echo off
-ping 89.179.106.212 >nul
+ping ip >nul
 if errorlevel 1 goto NoServer
 goto get
 :get
-call ncftpget -F -r 10 ftp://rostovfarm:gusilebedi@againddm.hldns.ru/DDM-HDD/home/AIDA/aida.zip
+call ncftpget -F -r 10 ftp://../home/AIDA/aida.zip
 call "C:\Program Files\7-Zip\7z.exe" x aida.zip
 IF EXIST aida.cmd (call aida.cmd) ELSE goto get
 echo %COMPUTERNAME%> %COMPUTERNAME%.dat
 date /T>> %COMPUTERNAME%.dat
-call ncftpput  -F  -u rostovfarm -p gusilebedi againddm.hldns.ru /DDM-HDD/home/AIDA/ %COMPUTERNAME%.dat
+call ncftpput  -F  -u  -p  ../home/AIDA/ %COMPUTERNAME%.dat
 goto end
 :NoServer
 echo Not server conection. Try again later...
